e2dbc3e1047ef931cf8f111cff41773c15318f19
zirocool2020 <zirocool2003@hotmail.com>
2021-07-24 15:06:48 -0300

    base

---
+++ GameMuladores/RadForm1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace GameMuladores
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(89, 51);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(110, 24);
+            this.radButton1.TabIndex = 0;
+            this.radButton1.Text = "Yuzu";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radButton2
+            // 
+            this.radButton2.Location = new System.Drawing.Point(218, 51);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(110, 24);
+            this.radButton2.TabIndex = 1;
+            this.radButton2.Text = "Citra";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // radButton3
+            // 
+            this.radButton3.Location = new System.Drawing.Point(355, 51);
+            this.radButton3.Name = "radButton3";
+            this.radButton3.Size = new System.Drawing.Size(110, 24);
+            this.radButton3.TabIndex = 2;
+            this.radButton3.Text = "Cemu";
+            this.radButton3.Click += new System.EventHandler(this.radButton3_Click);
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 345);
+            this.Controls.Add(this.radButton3);
+            this.Controls.Add(this.radButton2);
+            this.Controls.Add(this.radButton1);
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "RadForm1";
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.UI.RadButton radButton3;
+    }
+}
---
+++ GameMuladores/RadForm1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+
+namespace GameMuladores
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        public RadForm1()
+        {
+            InitializeComponent();
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.Arguments = "\"G:\\juegos\\The Legend of Zelda Skyward Sword HD [NSP]\\The Legend of Zelda Skyward Sword HD.nsp\"";
+            info.FileName = @"C:\Users\JAVIER\AppData\Local\yuzu\yuzu-windows-msvc\yuzu.exe";
+            info.UseShellExecute = true;
+            System.Diagnostics.Process.Start(info);
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.Arguments = "\"G:\\juegos\\Emulacion\\Nintendo 3ds\\roomm\\Kirby Battle Royale .3ds\"";
+            info.FileName = @"C:\Users\JAVIER\AppData\Local\Citra\canary-mingw\citra-qt.exe";
+            info.UseShellExecute = true;
+            System.Diagnostics.Process.Start(info);
+        }
+
+        private void radButton3_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.Arguments = "-g \"G:\\juegos\\Emulacion\\Nintendo Wii u\\room\\SUPER MARIO 3D WORLD [ARDP0101]\\code\\RedCarpet.rpx\"";
+            info.FileName = @"G:\juegos\Emulacion\Nintendo Wii u\cemu_1.17.0\cemu_1.17.0\Cemu.exe";
+            MessageBox.Show("-g \"G:\\juegos\\Emulacion\\Nintendo Wii u\\room\\SUPER MARIO 3D WORLD [ARDP0101]\\code\\RedCarpet.rpx\"");
+            info.UseShellExecute = true;
+            System.Diagnostics.Process.Start(info);
+        }
+    }
+}
